5b5f6eb3ff2ad8960d0e46314a6c34e32891e64f
Adi Akbar <adi.akbar91@gmail.com>
2020-08-17 21:37:37 +0700

    create person and planet component to show data

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import {useQuery} from 'react-query'
+import Person from './Person';
+
+const fetchPeople = async () => {
+  const res = await fetch('http://swapi.dev/api/people');
+  return res.json();
+}
 
 const People = () => {
+  const {data, status} = useQuery('people', fetchPeople)
+  
   return (
     <div>
-      This is people
+      <h2>People</h2>
+      {status === 'loading' && (
+        <div>Still Loading</div>
+      )}
+
+      {status === 'error' && (
+        <div>Error fetch data</div>
+      )}
+
+      {status === 'success' && (
+        data.results.map((person) => <Person key={person.name} person={person} />)
+      )}
     </div>
   )
 }
